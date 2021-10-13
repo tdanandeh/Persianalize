eb7b54da965107f9b0f9be684454698ce8f1e166
Tohid Danandeh <42702634+tdanandeh@users.noreply.github.com>
2021-10-13 09:13:56 +0330

    Add files via upload

---
+++ src/PersianalizeServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tdanandeh\Persianalize;
+
+use Illuminate\Support\ServiceProvider;
+
+class PersianalizeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
